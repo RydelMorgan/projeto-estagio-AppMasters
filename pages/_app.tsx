19065b543f4c9e8dc823e72fa7dc97207e0c5fd8
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2023-06-21 22:54:17 -0300

    Add MUI

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { AppProps } from "next/app";
+import theme from "../src/theme";
+
+
+
+
+
+export default function MyApp(props: AppProps) {
+  const { Component, pageProps } = props;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+      background: {
+        default: "#1D42CF"
+      },
+      text: {
+          primary: "#ffffff"
+      }
+    }
+  });
+  export default theme;
