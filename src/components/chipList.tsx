2b750855bc004d0dd727ff0405b9f42d0446597a
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2023-06-24 15:34:35 -0300

    Busca por gênero (#10)
    Closes #4

--- src/components/chipList.tsx
+++ src/components/chipList.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Chip from '@mui/material/Chip';
-import { Game } from '@/types';
+import { GameContext } from '@/context';
 
-export default function ChipList({ games }: { games: Game[] }) {
+export default function ChipList() {
+  const { games, setFilteredGames, isFilteredByTitle } = useContext(GameContext);
   const distinctGenres = Array.from(new Set(games.map((game) => game.genre)));
+  const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
+
+  const handleClick = (genre: string) => {
+    setSelectedGenres((prevSelectedGenres: string[]) => {
+      if (prevSelectedGenres.includes(genre)) {
+        return prevSelectedGenres.filter((selectedGenre) => selectedGenre !== genre);
+      } else {
+        return [...prevSelectedGenres, genre];
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (!isFilteredByTitle){
+      if (selectedGenres.length === 0) {
+        setFilteredGames(games);
+      } else {
+        const filteredGames = games.filter((game) => selectedGenres.includes(game.genre));
+        setFilteredGames(filteredGames);
+      }
+  }
+  }, [selectedGenres,isFilteredByTitle]);
 
   return (
     <Box
@@ -18,9 +41,14 @@ export default function ChipList({ games }: { games: Game[] }) {
         alignItems: 'center',
         gap: '10px',
       }}
-    > 
+    >
       {distinctGenres.map((genre, index) => (
-        <Chip key={index} label={genre} color="secondary"/>
+        <Chip
+          key={index}
+          label={genre}
+          onClick={() => handleClick(genre)}
+          color={selectedGenres.includes(genre) ? 'primary' : 'default'}
+        />
       ))}
     </Box>
   );
